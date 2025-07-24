3d5f245436f724366a80a4fb83897f1e2af008ce
David4976 <david160@hotmail.fr>
2025-07-24 15:42:25 +0200

    formatage exo3

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 let gestepossible = ["pierre", "feuille", "ciseaux"]
 
-<<<<<<< Updated upstream
-let gestePossible = Array ["Pierre", "Feuille", "Ciseaux"]
+let gestePossible = Array["Pierre", "Feuille", "Ciseaux"]
 const playButton = document.elementById("playButton");
 
-function getHumanChoice {
-playButton.addEventListener("click", () => {
-  let play = prompt("");
-  if (play === null) {
-    log.innerText = "Essayez à nouveau";
-  } else if (play.toLowerCase() === "") {
-    log.innerText = "Entrez Pierre, Feuille ou Ciseaux.";
-  } else if (play.toLowerCase() === gestePossible) {
-    return  (log.gestePossible.innerText);
+function getHumanChoice() {
+  playButton.addEventListener("click", () => {
+    let play = prompt("");
+    if (play === null) {
+      log.innerText = "Essayez à nouveau";
+    } else if (play.toLowerCase() === "") {
+      log.innerText = "Entrez Pierre, Feuille ou Ciseaux.";
+    } else if (play.toLowerCase() === gestePossible) {
+      return (log.gestePossible.innerText);
+    }
+    console.log(getHumanChoice)
   }
-  console.log (getHumanChoice)}
-=======
-function getComputerChoice () {
-    let indexaleatoire = Math.random () * 3
-
-console.log (indexaleatoire)
-
-   
-
-
- 
+  )
 }
 
-getComputerChoice ()
->>>>>>> Stashed changes
+
+
+// computer
+
+function getComputerChoice() {
+  let indexaleatoire = Math.random() * 3
+
+  console.log(indexaleatoire)
+}
+
+getComputerChoice()
