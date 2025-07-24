b33bc723a649c35f3eee2473259267c01f1b0114
Adelyx-creation <lyxadele@gmail.com>
2025-07-24 15:01:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hello World")
+
+let gestePossible = Array ["Pierre", "Feuille", "Ciseaux"]
+const playButton = document.elementById("playButton");
+
+function getHumanChoice {
+playButton.addEventListener("click", () => {
+  let play = prompt("");
+  if (play === null) {
+    log.innerText = "Essayez à nouveau";
+  } else if (play.toLowerCase() === "") {
+    log.innerText = "Entrez Pierre, Feuille ou Ciseaux.";
+  } else if (play.toLowerCase() === gestePossible) {
+    return  (log.gestePossible.innerText);
+  }
+  console.log (getHumanChoice)}
