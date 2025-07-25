fe374b45f74cd4a26a583efce25b5c0f665201a2
Melanie288 <melanie.devy@gmail.com>
2025-07-25 10:09:24 +0200

    logiqordihumain ok

--- script.js
+++ script.js
@@ -1,31 +1,36 @@
-let gestepossible = ["pierre", "feuille", "ciseaux"]
-
-let gestePossible = Array["Pierre", "Feuille", "Ciseaux"]
-const playButton = document.elementById("playButton");
-
-function getHumanChoice() {
-  playButton.addEventListener("click", () => {
-    let play = prompt("");
-    if (play === null) {
-      log.innerText = "Essayez à nouveau";
-    } else if (play.toLowerCase() === "") {
-      log.innerText = "Entrez Pierre, Feuille ou Ciseaux.";
-    } else if (play.toLowerCase() === gestePossible) {
-      return (log.gestePossible.innerText);
-    }
-    console.log(getHumanChoice)
-  }
-  )
-}
-
-
-
 // computer
-
+let gestepossible = ["pierre", "feuille", "ciseaux"]
 function getComputerChoice() {
-  let indexaleatoire = Math.random() * 3
-
-  console.log(indexaleatoire)
+  let indexaleatoire = Math.floor(Math.random() * 3)
+ const gestealeatoire = (gestepossible [indexaleatoire])
+  console.log(gestealeatoire)
 }
 
 getComputerChoice()
+
+// human exo 3
+
+//let gestepossible = ["pierre", "feuille", "ciseaux"]
+gestepossible [0]
+//console.log(gestepossible [0])
+
+// const playButton = document.elementById("playButton");
+
+function getHumanChoice() {
+//  playButton.addEventListener("click", () => {
+
+    let play = prompt ("Jouez Pierre Feuille ou ciseaux");
+console.log (play)
+  //  if (play === null) {
+   //   log.innerText = "Essayez à nouveau";
+   // } else if (play.toLowerCase() === "") {
+   //   log.innerText = "Entrez Pierre, Feuille ou Ciseaux.";
+   // } else if (play.toLowerCase() === gestePossible) {
+   //   return (log.gestePossible.innerText);
+    }
+    
+ getHumanChoice ()
+  
+
+
+
