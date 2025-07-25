d23649055d2e7bef4105b551c7984edd877233d2
Melanie288 <melanie.devy@gmail.com>
2025-07-25 11:36:24 +0200

    jeu ok

--- script.js
+++ script.js
@@ -1,34 +1,51 @@
 // computer
 let gestepossible = ["pierre", "feuille", "ciseaux"]
+
 function getComputerChoice() {
   let indexaleatoire = Math.floor(Math.random() * 3)
  const gestealeatoire = (gestepossible [indexaleatoire])
   console.log(gestealeatoire)
+  return gestealeatoire
 }
-
-getComputerChoice()
+let gestealeatoire = getComputerChoice()
 
 // human exo 3
 
-//let gestepossible = ["pierre", "feuille", "ciseaux"]
+
 gestepossible [0]
 //console.log(gestepossible [0])
 
-// const playButton = document.elementById("playButton");
 
 function getHumanChoice() {
-//  playButton.addEventListener("click", () => {
+  
 
     let play = prompt ("Jouez Pierre Feuille ou ciseaux");
 console.log (play)
-  //  if (play === null) {
-   //   log.innerText = "Essayez à nouveau";
-   // } else if (play.toLowerCase() === "") {
-   //   log.innerText = "Entrez Pierre, Feuille ou Ciseaux.";
-   // } else if (play.toLowerCase() === gestePossible) {
-   //   return (log.gestePossible.innerText);
+
+  if (play == "ciseaux" && gestealeatoire == "feuille"){
+    console.log ("Tu as gagné")
+   }
+  else if (play == "feuille" && gestealeatoire == "pierre"){
+    console.log ("Tu as gagné")
+}
+  else if (play == "pierre" && gestealeatoire == "ciseaux"){
+    console.log ("Tu as gagné")
+}
+ else if (play == gestealeatoire) {
+   console.log ("égalité")
+}
+  else if (play == "pierre" && gestealeatoire == "feuille"){
+    console.log ("victoire de l'ordi")
+}
+  else if (play == "feuille" && gestealeatoire == "ciseaux"){
+   console.log ("victoire de l'ordi")
+  }
+  else if (play == "ciseaux" && gestealeatoire == "pierre"){
+   console.log ("victoire de l'ordi")
     }
+ }
     
+  console.log ()   
  getHumanChoice ()
   
 
